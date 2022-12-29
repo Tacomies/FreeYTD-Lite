31a0bda2f9c0b5a3564a7100833f65fa2bce6a88
kjsales <saleskarri@gmail.com>
2022-12-29 20:19:14 +0200

    LiteYTD v:2.0.0

--- LiteYTD.py
+++ LiteYTD.py
@@ -1,45 +1,76 @@
 from tkinter import *
 from tkinter import messagebox
+from tkinter import ttk
 from pytube import YouTube
 from pathlib import Path
+import os
 window = Tk()
 
-#Main function for downloading video and catching errors during the process
-def download_video(link):
-    path = Path(__file__).with_name("downloads")
-    if link:
+def download_video(vid, path):
+    while True:
         try:
-            video = YouTube(link)
-        except: #Catches invalid links
-            messagebox.showerror(title="Link error", message="Invalid URL\nMake sure video exists")
-            return None
-        while True:
-            try: #Retries download if YouTube server fails
-                video = video.streams.get_highest_resolution()
-                video.download(path)
-                break
-            except:
-                continue
-        messagebox.showinfo(title=None, message=f"Video downloaded to {path}")
-    else: #Catches download attempt with no given URL
-        messagebox.showerror(title="Link error", message="URL can't be empty")
-#Function for setting up the GUI
-def setup():
+            video = vid.streams.get_highest_resolution()
+            video.download("downloads")
+            messagebox.showinfo(title="Download successful", message=f"Video downloaded to\n{path}")
+            break
+        except:
+            continue
+
+def download_audio(aud, path):
+    while True:
+        try:
+            audio = aud.streams.filter(only_audio=True).first()
+            audio = audio.download("downloads")
+            base, ext = os.path.splitext(audio)
+            new_file = base + '.mp3'
+            os.rename(audio, new_file)
+            messagebox.showinfo(title="Download successful", message=f"Audio downloaded to\n{path}")
+            break
+        except:
+            continue
+
+def get_video(link):
+    try:
+        video = YouTube(link)
+        return video
+    except:
+        messagebox.showerror(title="URL ERROR", message="Make sure that video URL is neither empty or invalid")
+
+def selection(option, link):
+    path = Path(__file__).with_name("downloads")
+    video = get_video(link)
+    if video:
+        match(option):
+            case "Mp4":
+                download_video(video, path)
+            case "Mp3":
+                download_audio(video, path)
+            case _:
+                messagebox.showerror(title="FORMAT ERROR", message="Select format!")
+
+    
+def setup(): #Function for setting up the GUI
     window.title("FreeYTD")
     wd = window.winfo_screenwidth()/2
     hd = window.winfo_screenheight()/2
     window.geometry('%dx%d+%d+%d' % (225, 45, wd-125, hd-125))
-    linkfield = Entry(window, width=35)
+    window.resizable(False, False)
+    linkfield = Entry(window, width=24)
+    options = ["Mp4", "Mp3"]
+    select = ttk.Combobox(window, value=options, width=5)
+    select.place(x=175)
     Label(window, text="Link:").grid(row=0)
     linkfield.grid(row=0, column=1)
-    btn = Button(window, text="DOWNLOAD", command=lambda: download_video(linkfield.get()))
+    btn = Button(window, text="DOWNLOAD", command=lambda: selection(select.get(), linkfield.get()))
     btn.place(x=0, y=20)
     tag = Label(window, text = "DC: Tacomies#2616")
     tag.config(font =("Courier", 10))
     tag.place(x=85, y=20)
-#Main function for style reasons
-def main():
+
+def main(): #Main function for style reasons
     setup()
     mainloop()
 
-main()
+
+if __name__ == "__main__":
+    main()
